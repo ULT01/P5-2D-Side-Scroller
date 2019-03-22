b7fff4899d1bb5884f1df8bf4d01f1738c64e79c
ULT01 <47332107+ULT01@users.noreply.github.com>
2019-03-22 15:12:28 -0400

    Update sketch.js
    make changes:
    
    1. define a variable known as "score" to determine the final score
    2. define "SceneNum" in order to change scenes.
    3. change the y-value of the "jump" vector to "-3.5"

--- sketch.js
+++ sketch.js
@@ -1,50 +1,113 @@
-
-sketch.js
-
-// Daniel Shiffman
-// https://www.kadenze.com/courses/the-nature-of-code
-// http://natureofcode.com/
-// Session 2: Array of Particles, multiple forces
-
 var person;
 
-var obstacle;
+
+var sceneNum = 0;
+
+var score = 0;
+
+
 
 function setup() {
   createCanvas(640, 360);
+
   person = new Person();
+
   
+
 }
+
+
 
 function keyPressed() {
+
   if (key === ' ') {
-    var jump = createVector(0, -1);
+
+    var jump = createVector(0, -3.5);
+
     	person.applyForce(jump);
+    
+    score ++;
+
+  }			 else if (key == 'q') {
+    sceneNum ++;
   }
+  
+  	else if (key == 'x') {
+      sceneNum += 2;
+    }
+  
+		else if (key == 'r') {
+      sceneNum = 0;
+    }
 }
+
+
 
 function draw() {
-  background(51);
   
+  if (sceneNum === 0) {
+    background(255, 185, 0);
+    textSize(20);
+    fill(255, 50, 205);
+    text('Press "q" to play', 200, 150);
+    text('Press "space" to jump', 200, 250);
+  } else if (sceneNum===1){
+  
+
+  background(255, 185, 0);
+
+  
+
   translate(-person.pos.x + 50, 0);
+
   var gravity = createVector(0, 0.1);
+
   person.applyForce(gravity);
+
   
+
   person.update();
+
   person.edges();
-  person.display();
-  
-  
-  if (mouseIsPressed) {
     
-  var force = createVector(-0.1, 0);
+        
+	person.display();
+    
+   
+
+  if (mouseIsPressed) {
+
+    
+
+  var force = createVector(0, -0.01);
+
   person.applyForce(force);
+
   
+
   }
+
   // translate(-100, 0);
+
 		
+
 	fill(255, 0, 100);
-  rect(400, height - 50, 50, 50);
-  
+
+  ellipse(width , height - 15, 30, 30);
+} else { 
+			background(0, 0, 0);
+  		noStroke();
+  		fill(255, 0, 0);
+  		text('Final Score: ' + score, 50, 50);
+  		textSize(25);
+  		text('Game Over!!!', 235, 200);
+  		
+  		
+	}
 }
 
+
+//  mouseClicked = function() {
+//     person.growBy(10);
+//     person.display();
+// };
